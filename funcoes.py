1160cb490622a2ab0937e6a51ce6d0561b4107e5
joaomge01 <joaomge@al.insper.edu.br>
2025-10-22 14:39:40 -0300

    Update funcoes.py
    EP2(Question 4)

--- funcoes.py
+++ funcoes.py
@@ -24,4 +24,41 @@ def faz_jogada(tabuleiro, linha, coluna):
         tabuleiro[linha][coluna] = "-"
     return tabuleiro
 
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicoes = []
+    if orientacao == "vertical":
+        for i in range(tamanho):
+            posicoes.append([linha + i, coluna])
+    else:
+        for i in range(tamanho):
+            posicoes.append([linha, coluna + i])
+    return posicoes
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    if nome_navio in frota:
+        frota[nome_navio].append(posicoes)
+    else:
+        frota[nome_navio] = [posicoes]
+    return frota
+
+def faz_jogada(tabuleiro, linha, coluna):
+    if tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = "X"
+    else:
+        tabuleiro[linha][coluna] = "-"
+    return tabuleiro
+
+
+def posiciona_frota(frota):
+    tabuleiro = [[0 for _ in range(10)] for _ in range(10)]
+    for navio in frota:
+        for posicoes in frota[navio]:
+            for pos in posicoes:
+                linha, coluna = pos
+                tabuleiro[linha][coluna] = 1
+    return tabuleiro
+
+
+
 
